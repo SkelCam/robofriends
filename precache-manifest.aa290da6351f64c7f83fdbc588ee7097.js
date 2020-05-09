92a922d0616bc85f9fdfc9a463a9cf7c715f5d1a
SkelCam <edd.skelton@gmail.com>
2020-05-09 09:00:19 +0100

    Updates

---
+++ precache-manifest.aa290da6351f64c7f83fdbc588ee7097.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6840a529eaf8d22781fcc687472c2e65",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "155520c129548449f706",
+    "url": "/robofriends/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "13310e7b7ea481870b5b",
+    "url": "/robofriends/static/css/main.63862961.chunk.css"
+  },
+  {
+    "revision": "155520c129548449f706",
+    "url": "/robofriends/static/js/2.d82cd422.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.d82cd422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13310e7b7ea481870b5b",
+    "url": "/robofriends/static/js/main.f569911b.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
